fbf78955f02dae5e630453bf27e26e16f8286bbd
jhonas <jhonas.burlamaqui@gmail.com>
2025-06-14 18:51:36 +0100

    accpting default config file

--- includes/webserv.hpp
+++ includes/webserv.hpp
@@ -1,14 +1,16 @@
 #pragma once
 #include <cstring>
+#include <exception>
 #include <errno.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/select.h>
+#include <stdexcept>
+#include <stdexcept>
 #include <unistd.h>
 #include <iostream>
-#include <stdexcept>
 #include <map>
 #include <csignal>
 
